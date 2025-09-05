28da7a8bc58b8f33c9cf1c73e9c7d1fdbe65fbd3
Anabol1ks <grigorogannisyan.12@gmail.com>
2025-09-05 09:24:18 +0300

    new

--- semestr-5/Testirovanie/pr2/calculate/go.mod
+++
@@ -1,3 +0,0 @@
-module calcul
-
-go 1.23.0
--- semestr-5/toiv/8_2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("sensor_data.csv")
-plt.hist(data["CO2"], bins=10, edgecolor='black')
-plt.title("Гистограмма частоты показаний CO2")
-plt.xlabel("CO2 (ppm)")
-plt.ylabel("Частота")
-plt.show()
